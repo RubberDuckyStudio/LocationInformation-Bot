7d6bc74e06dd38d65ac28f3e2f659113051564e4
11775815 <e11775815@student.tuwien.ac.at>
2019-12-10 15:03:04 +0100

    get city information by long- and latitude

---
+++ src/main/java/won/bot/skeleton/location/City.java
@@ -0,0 +1,52 @@
+package won.bot.skeleton;
+
+public class City {
+    private double longitude;
+    private double latitude;
+    private String name;
+    private String country;
+    private String region;
+
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+
+}
---
+++ src/main/java/won/bot/skeleton/location/GDS.java
@@ -0,0 +1,68 @@
+package won.bot.skeleton;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.jose4j.json.internal.json_simple.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GDS {
+    String apiKey="X6RG20EXBIOOERD2YNTZDHK6XTCWNN3L";
+
+    public List<City> getCityByLngLat(double lng, double lat){
+        return jsonToCity(getCityByLngLat(lng,lat));
+    }
+
+    public List<String> getCitiesInJSON(double lng, double lat){
+        try {
+            URL url = new URL("https://api.geodatasource.com/city?key="+apiKey+"&format=json&lat="+lat+"&lng="+lng);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            List<String> ret= new ArrayList<>();
+            while ((output = br.readLine()) != null) {
+                ret.add(output);
+            }
+            conn.disconnect();
+            return ret;
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<City> jsonToCity(List<String> jsonCities){
+        List<City> ret= new ArrayList<>();
+        for (String str:jsonCities) {
+            City tmpCity=new City();
+            JsonObject jsonObject = new JsonParser().parse(str).getAsJsonObject();
+
+            tmpCity.setCountry(jsonObject.get("country").toString());
+            tmpCity.setName(jsonObject.get("city").toString());
+            tmpCity.setRegion(jsonObject.get("region").toString());
+            tmpCity.setLatitude(jsonObject.get("latitude").getAsDouble());
+            tmpCity.setLongitude(jsonObject.get("longitude").getAsDouble());
+
+            ret.add(tmpCity);
+        }
+        return ret;
+    }
+}

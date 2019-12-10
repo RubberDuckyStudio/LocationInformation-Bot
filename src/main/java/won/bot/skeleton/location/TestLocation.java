c88bed6d9eaaf96657de12b2549ec7a2a885997e
11775815 <e11775815@student.tuwien.ac.at>
2019-12-10 15:38:30 +0100

    added test for GDS

---
+++ src/main/java/won/bot/skeleton/location/TestLocation.java
@@ -0,0 +1,8 @@
+package won.bot.skeleton.location;
+
+public class TestLocation {
+    public static void main(String[] args) {
+        GDS testGDS = new GDS();
+        System.out.println(testGDS.getCityByLngLat(-122.084,37.3861));
+    }
+}
